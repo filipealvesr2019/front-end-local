46430022385436aa243b70154be46eeaa13cafaf
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2024-07-24 13:07:48 -0300

    refactor: estiliza landing page

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -44,28 +44,89 @@ const LandingPage = () => {
       </header>
       <main className={styles.main}>
         <section id="sobre">
-          <h2>Sobre Nós</h2>
-          <p>Informações sobre a empresa ou produto.</p>
+          <h2 className={styles.h2}>
+          Transforme Seu Delivery com Nossa Solução Completa
+
+
+          </h2>
+          <p className={styles.p}>
+          Descubra o futuro da gestão de delivery com o nosso sistema de assinatura inovador. Combinando um ERP robusto e um site local personalizado, oferecemos uma solução tudo-em-um que vai além das expectativas.
+          </p>
         </section>
         <section id="servicos">
-          <h2>Serviços</h2>
-          <p>Detalhes sobre os serviços oferecidos.</p>
+          <h2 className={styles.h2}>Com nosso sistema, você poderá:</h2>
+          <p className={styles.p}>
+            <b>Gerenciar Pedidos e Estoque: </b> Controle suas operações com facilidade, otimizando o gerenciamento de pedidos e o estoque em tempo real.
+
+          </p>
+
+          <p className={styles.p}>
+            <b>Personalizar Seu Site de Delivery: </b> Tenha um site local adaptado às suas necessidades, garantindo uma experiência de usuário intuitiva e eficiente.
+          </p>
+
+          <p className={styles.p}>
+            <b>Automatizar Processos: </b> Automatize tarefas repetitivas e ganhe mais tempo para focar no crescimento do seu negócio.
+          </p>
+          <p className={styles.p}>
+            <b>Analisar Desempenho: </b> Acompanhe métricas detalhadas e tome decisões informadas para aprimorar seu serviço.
+          </p>
+          <button className={styles.button}>Comece Hoje</button>
         </section>
         <section id="contato">
-          <h2>Contato</h2>
+          <h2 className={styles.h2}>Contato</h2>
           <p>Formulário ou informações de contato.</p>
         </section>
         <section id="sobre">
-          <h2>Quem Somos</h2>
-          <p>Informações sobre a empresa ou produto.</p>
+          <h2 className={styles.h2}>Quem Somos</h2>
+          <p>
+          Somos uma equipe dedicada a revolucionar o setor de delivery com soluções tecnológicas avançadas. Nossa missão é proporcionar ferramentas que ajudam empresas a crescerem e a oferecerem um serviço excepcional aos seus clientes.
+          </p>
+          <h2 className={styles.h2}>Nossos Valores:</h2>
+
+          <p className={styles.p}>
+            <b>Inovação: </b> Estamos sempre em busca de novas soluções para
+            aprimorar sua experiência.
+          </p>
+
+          <p className={styles.p}>
+            <b>Facilidade de Uso: </b> Nossos produtos são projetados para serem
+            intuitivos e acessíveis a todos.
+          </p>
+          <p className={styles.p}>
+            <b>Suporte ao Cliente: </b> Oferecemos suporte dedicado para
+            ajudá-lo em cada etapa do caminho.
+          </p>
         </section>
-        <section id="servicos">
-          <h2>Serviços</h2>
-          <p>Detalhes sobre os serviços oferecidos.</p>
-        </section>
-        <section id="contato">
-          <h2>Contato</h2>
-          <p>Formulário ou informações de contato.</p>
+
+        <section id="contato" className={styles.contato}>
+          <h2 className={styles.h2}>Contato</h2>
+
+          <div>
+            <h2 className={styles.h3}>Fale Conosco</h2>
+            <p className={styles.p}>
+              <b>Email: </b> contato@[nomedaempresa].com
+            </p>
+            <p className={styles.p}>
+              <b>Telefone: </b> (11) 1234-5678
+            </p>
+            <p className={styles.p}>
+              <b>Endereço: </b> Rua Exemplo, 123, São Paulo, SP
+            </p>
+          </div>
+
+          <div>
+            <h2 className={styles.h3}>Links Úteis</h2>
+            <p className={styles.p}>Quem Somos</p>
+            <p className={styles.p}>Planos</p>
+            <p className={styles.p}>Contato</p>
+          </div>
+
+          <div>
+            <h2 className={styles.h3}>Redes Sociais</h2>
+            <p className={styles.p}>Facebook</p>
+            <p className={styles.p}>Instagram</p>
+            <p className={styles.p}>LinkedIn</p>
+          </div>
         </section>
       </main>
       <footer>
