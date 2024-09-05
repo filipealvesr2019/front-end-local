91f23857d1f93c8dc51cefe8dda1b50cdba485c9
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2024-09-05 20:29:49 -0300

    refactor: adiciona fetch de produtos.

--- src/ecommerce/Products/ProductsGET.jsx
+++ src/ecommerce/Products/ProductsGET.jsx
@@ -1,86 +1,59 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 import { useConfig } from "../context/ConfigContext";
 import styles from "./ProductsGET.module.css";
+
 export default function Products() {
   const { apiUrl } = useConfig();
-  const [data, setData] = useState([]); // Initialize as an empty array
+  const [data, setData] = useState([]);
   const [selectedVariations, setSelectedVariations] = useState({});
 
-  async function getProducts() {
+  const getProducts = useCallback(async () => {
     try {
       const response = await axios.get(`${apiUrl}/api/products`);
-      setData(response.data || []); // Ensure data is an array
+      setData(response.data || []);
     } catch (error) {
       console.error("Error fetching products:", error);
-      setData([]); // Set an empty array in case of error
+      setData([]);
     }
-  }
+  }, [apiUrl]);
 
   useEffect(() => {
     getProducts();
-  }, []);
+  }, [apiUrl]);
 
-  const handleVariation = (productId, variation,  index) => {
-   
-
+  const handleVariation = useCallback((productId, variation, index) => {
     const key = `${productId}-${index}`;
     setSelectedVariations((prevState) => {
-        if(prevState[key]){
-            const {[key]: _, ...rest} = prevState
-            console.log('variação apagada');
-            return rest
-        } else {
-              // Otherwise, add the variation
+      if (prevState[key]) {
+        const { [key]: _, ...rest } = prevState;
+        console.log('variação apagada');
+        return rest;
+      } else {
         console.log("price", variation.price);
         console.log("name", variation.name);
         console.log("url", variation.url);
-
-        return {...prevState, [key]: variation}
-        }
+        return { ...prevState, [key]: variation };
+      }
     });
-  };
+  }, [setSelectedVariations]);
 
   return (
     <div style={{ marginTop: "25rem" }}>
       {data.length > 0 ? (
         data.map((product) => (
-          <div
-            key={product._id}
-            style={{
-              marginTop: "10rem",
-            }}
-          >
+          <div key={product._id} style={{ marginTop: "10rem" }}>
             {product.name}
-            <img
-              src={product.imageUrl}
-              alt={product.name}
-              style={{
-                width: "15vw",
-              }}
-            />
+            <img src={product.imageUrl} alt={product.name} style={{ width: "15vw" }} />
             <div>
               {product.variations && product.variations.length > 0 ? (
                 product.variations.map((variation, index) => (
                   <div key={index} className={styles.variationContainer}>
-                    <img
-                      src={variation.url}
-                      alt={variation.name}
-                      style={{
-                        width: "15vw",
-                      }}
-                    />
+                    <img src={variation.url} alt={variation.name} style={{ width: "15vw" }} />
                     <p>{variation.name}</p>
                     <p>R${variation.price}</p>
-                    <span
-                      onClick={() =>
-                        handleVariation(product._id, variation, index)
-                      }
-                    >
-                      {" "}
-                      {selectedVariations[`${product._id}-${index}`]
-                        ? "-"
-                        : "+"}
+                    <span onClick={() => handleVariation(product._id, variation, index)}>
+                      {selectedVariations[`${product._id}-${index}`] ? "-" : "+"}
                     </span>
                   </div>
                 ))
