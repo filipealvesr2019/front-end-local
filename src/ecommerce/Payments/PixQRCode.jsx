213df557ef7bc1414517ed4e6e987fac42990f04
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2024-09-13 12:34:40 -0300

    feat: adiciona rota de de busca ecommerce e produto pro id da loja e pelo id do admin

--- src/ecommerce/Payments/PixQRCode.jsx
+++ src/ecommerce/Payments/PixQRCode.jsx
@@ -2,17 +2,20 @@ import { useEffect, useState } from "react";
 import { useConfig } from "../context/ConfigContext";
 import axios from "axios";
 import { useAtom } from "jotai";
+import { storeID } from "../../../store/store";
+import Cookies from "js-cookie";
+
 
 export default function PixQRCode() {
   const [qrcode, setQrcode] = useState("");
   const { apiUrl } = useConfig();
-  const [storeID] = useAtom(storeID);
+  const [ecommerceID, setEcommerceID] = useAtom(storeID); // Use corretamente o atom
 
   useEffect(() => {
-    async function getPix() {
+    async function getPix(id) {
       try {
         
-        const response = await axios.get(`${apiUrl}/api/pix/ecommerce/${storeID}/qrcode`);
+        const response = await axios.get(`${apiUrl}/api/pix/ecommerce/${id}/qrcode`);
         setQrcode(response.data);
         console.log("qrcode", response.data);
       } catch (error) {
@@ -22,6 +25,24 @@ export default function PixQRCode() {
     }
     getPix();
   }, []);
+
+
+  useEffect(() => {
+    // Recupera o storeID do cookie
+    const savedStoreID = Cookies.get("storeID");
+
+    if (savedStoreID) {
+      setEcommerceID(savedStoreID); // Atualiza o atom com o storeID salvo no cookie
+      setQrcode(savedStoreID); // Usa o storeID para buscar os produtos
+      console.log("storeID recuperado dos cookies:", savedStoreID);
+    } else if (ecommerceID) {
+      setQrcode(ecommerceID); // Se não tiver cookie, usa o atom
+      console.log("storeID do Atom:", ecommerceID);
+    } else {
+      console.log("storeID não disponível");
+      setData([]); // Limpa os produtos se o storeID não estiver disponível
+    }
+  }, [ecommerceID, setEcommerceID]); // Dependência do ecommerceID
   return (
     <>
       {qrcode && (
