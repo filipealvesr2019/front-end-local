15ec71cc3f3e1b6d5ae38a515685122c9e8fe46b
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2024-09-13 12:38:26 -0300

    refactor: refatora codigo de mostrar QR code.

--- src/ecommerce/Payments/PixQRCode.jsx
+++ src/ecommerce/Payments/PixQRCode.jsx
@@ -5,52 +5,53 @@ import { useAtom } from "jotai";
 import { storeID } from "../../../store/store";
 import Cookies from "js-cookie";
 
-
 export default function PixQRCode() {
-  const [qrcode, setQrcode] = useState("");
+  const [qrcode, setQrcode] = useState(null);
   const { apiUrl } = useConfig();
-  const [ecommerceID, setEcommerceID] = useAtom(storeID); // Use corretamente o atom
+  const [ecommerceID, setEcommerceID] = useAtom(storeID);
 
   useEffect(() => {
     async function getPix(id) {
+      if (!id) return; // Evitar chamadas desnecessárias
+
       try {
-        
         const response = await axios.get(`${apiUrl}/api/pix/ecommerce/${id}/qrcode`);
         setQrcode(response.data);
         console.log("qrcode", response.data);
       } catch (error) {
-        console.error("Error fetching products:", error);
+        console.error("Error fetching QR Code:", error);
         setQrcode(null);
       }
     }
-    getPix();
-  }, []);
 
+    if (ecommerceID) {
+      getPix(ecommerceID); // Chama a função com o ID correto
+    }
+  }, [ecommerceID, apiUrl]);
 
   useEffect(() => {
-    // Recupera o storeID do cookie
     const savedStoreID = Cookies.get("storeID");
 
     if (savedStoreID) {
-      setEcommerceID(savedStoreID); // Atualiza o atom com o storeID salvo no cookie
-      setQrcode(savedStoreID); // Usa o storeID para buscar os produtos
+      setEcommerceID(savedStoreID); // Atualiza o Atom com o storeID salvo no cookie
       console.log("storeID recuperado dos cookies:", savedStoreID);
     } else if (ecommerceID) {
-      setQrcode(ecommerceID); // Se não tiver cookie, usa o atom
       console.log("storeID do Atom:", ecommerceID);
     } else {
       console.log("storeID não disponível");
-      setData([]); // Limpa os produtos se o storeID não estiver disponível
+      setQrcode(null); // Limpa o QR code se o storeID não estiver disponível
     }
-  }, [ecommerceID, setEcommerceID]); // Dependência do ecommerceID
+  }, [ecommerceID, setEcommerceID]);
+
   return (
     <>
-      {qrcode && (
+      {qrcode ? (
         <>
-          {" "}
-          <img src={qrcode.qrCodeUrl} alt="" />
+          <img src={qrcode.qrCodeUrl} alt="QR Code" />
           <span>{qrcode.pixKey}</span>
         </>
+      ) : (
+        <p>QR Code não disponível.</p>
       )}
     </>
   );
