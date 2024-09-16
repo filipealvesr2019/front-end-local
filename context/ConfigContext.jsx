1ed111d8c14036de6e40936bf305a1e19ba71e04
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2024-09-16 11:05:24 -0300

    feat: adiciona url

--- context/ConfigContext.jsx
+++ context/ConfigContext.jsx
@@ -1,12 +1,12 @@
 // ConfigContext.js
 import React, { createContext, useState } from 'react';
 const localhost = 'http://localhost:3002'
-const apiUrl = 'https://b06e10aeb2f1deb5e9fc2c0bf1a3dde2.serveo.net';
+const apiUrl = 'https://64f22388c1f395658e3d59cd7fc11988.serveo.net';
 export const ConfigContext = createContext();
 
 export const ConfigProvider = ({ children }) => {
   const [config, setConfig] = useState({
-    apiUrl: localhost,
+    apiUrl: apiUrl,
   });
 
 
