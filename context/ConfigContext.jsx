9718f8a20ac62b0019c8aeee0243d4b1e01f46af
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2024-09-16 10:55:22 -0300

    feat: adiciona url

--- context/ConfigContext.jsx
+++ context/ConfigContext.jsx
@@ -1,12 +1,12 @@
 // ConfigContext.js
 import React, { createContext, useState } from 'react';
 const localhost = 'http://localhost:3002'
-const ngrok = "https://d87e-2804-1a04-804f-e800-ccba-cd15-961b-6945.ngrok-free.app"
+const apiUrl = 'https://64f22388c1f395658e3d59cd7fc11988.serveo.net/';
 export const ConfigContext = createContext();
 
 export const ConfigProvider = ({ children }) => {
   const [config, setConfig] = useState({
-    apiUrl: ngrok,
+    apiUrl: apiUrl,
   });
 
 
