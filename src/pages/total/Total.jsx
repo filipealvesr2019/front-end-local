a8e8d7cde81b4a59e201b4db7520accd7679920a
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2024-09-17 21:04:28 -0300

    feat: adiciona logica de adicionar despesas e receitas

--- src/pages/total/Total.jsx
+++ src/pages/total/Total.jsx
@@ -1,24 +1,28 @@
 import "./Total.css";
-import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
-import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
+import ArrowUpwardIcon from "@mui/icons-material/ArrowUpward";
+import ArrowDownwardIcon from "@mui/icons-material/ArrowDownward";
 import Cookies from "js-cookie";
 import { useConfig } from "../../../context/ConfigContext";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
-export default function Total({storeID}) {
 
+export default function Total({ storeID }) {
   const { apiUrl } = useConfig();
   const [day, setDay] = useState(0);
   const [month, setMonth] = useState(0);
-  const [year, setYear] = useState(0);
+  const [monthPercentageChange, setMonthPercentageChange] = useState(0);
 
-  
+  const [year, setYear] = useState(0);
+  const AdminID = Cookies.get("AdminID"); // Obtenha o ID do cliente do cookie
+
   console.log("storeID", storeID);
 
   async function getTotalDay() {
     try {
-      const response = await axios.get(`${apiUrl}/api//admin/vendas/total-dia/${storeID}`);
+      const response = await axios.get(
+        `${apiUrl}/api//admin/vendas/total-dia/${storeID}`
+      );
       setDay(response.data.totalGanho);
     } catch (error) {
       console.error("Error fetching products:", error);
@@ -27,8 +31,12 @@ export default function Total({storeID}) {
   }
   async function getTotalMonth() {
     try {
-      const response = await axios.get(`${apiUrl}/api/admin/vendas/total-mes/${storeID}`);
-      setMonth(response.data.totalGanho);
+      const response = await axios.get(
+        `${apiUrl}/api/profit-percentage/${AdminID}`
+      );
+      setMonth(response.data.currentMonthProfit);
+      console.log(response.data.percentageChange);
+      setMonthPercentageChange(response.data.percentageChange);
     } catch (error) {
       console.error("Error fetching products:", error);
       setData([]);
@@ -37,7 +45,9 @@ export default function Total({storeID}) {
 
   async function getTotalYear() {
     try {
-      const response = await axios.get(`${apiUrl}/api/admin/vendas/total-mes/${storeID}`);
+      const response = await axios.get(
+        `${apiUrl}/api/admin/vendas/total-mes/${storeID}`
+      );
       setYear(response.data.totalGanho);
     } catch (error) {
       console.error("Error fetching products:", error);
@@ -47,12 +57,12 @@ export default function Total({storeID}) {
   // Só faz a requisição quando o storeID for válido
   useEffect(() => {
     if (storeID) {
-        console.log("Fetching products com storeID:", storeID);
-        getTotalDay();
-        getTotalMonth()
-        getTotalYear()
+      console.log("Fetching products com storeID:", storeID);
+      getTotalDay();
+      getTotalMonth();
+      getTotalYear();
     }
-}, [storeID]);
+  }, [storeID]);
 
   return (
     <div className="featured">
@@ -61,7 +71,8 @@ export default function Total({storeID}) {
         <div className="featuredMoneyContainer">
           <span className="featuredMoney">R$ {day}</span>
           <span className="featuredMoneyRate">
-            -11.4 <ArrowDownwardIcon  className="featuredIcon negative"/>
+            percentageChange{" "}
+            <ArrowDownwardIcon className="featuredIcon negative" />
           </span>
         </div>
         <span className="featuredSub">Compared to last month</span>
@@ -71,17 +82,27 @@ export default function Total({storeID}) {
         <div className="featuredMoneyContainer">
           <span className="featuredMoney">R$ {month}</span>
           <span className="featuredMoneyRate">
-            -1.4 <ArrowDownwardIcon className="featuredIcon negative"/>
+            {monthPercentageChange >= 0 ? (
+              <>
+                {monthPercentageChange.toFixed(2)}%
+                <ArrowUpwardIcon className="featuredIcon" />
+              </>
+            ) : (
+              <>
+                {monthPercentageChange.toFixed(2)}%
+                <ArrowDownwardIcon className="featuredIcon negative" />
+              </>
+            )}
           </span>
         </div>
-        <span className="featuredSub">Compared to last month</span>
+        <span className="featuredSub">Comparado ao ultimo mes</span>
       </div>
       <div className="featuredItem">
         <span className="featuredTitle">Ano</span>
         <div className="featuredMoneyContainer">
           <span className="featuredMoney">R$ {year}</span>
           <span className="featuredMoneyRate">
-            +2.4 <ArrowUpwardIcon className="featuredIcon"/>
+            +2.4 <ArrowUpwardIcon className="featuredIcon" />
           </span>
         </div>
         <span className="featuredSub">Compared to last month</span>
